990ca78382a9a9d5d014d4b8b95921e6f5d0b710
Roz <rosalindavalbrecht@gmail.com>
2021-10-15 17:06:04 -0700

    added date picker but not working need additional packages for functionality with mui

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { DateTimePicker, MuiPickersUtilsProvider } from "@mui/lab";
 
 export default function Home() {
   const {
@@ -12,16 +13,24 @@ export default function Home() {
 
   console.log(watch("example")); // watch input value by passing the name of it
 
+  const dtPicker = (data) => console.log("got to date time picker", data);
+
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        {/* register your input into the hook by invoking the "register" function */}
-        <input defaultValue="test" {...register("example")} />
-
-        {/* include validation with required or other standard HTML validation rules */}
-        <input {...register("exampleRequired", { required: true })} />
-        {/* errors will return when field validation fails  */}
-        {errors.exampleRequired && <span>This field is required</span>}
+        <input
+          defaultValue="Tracker Name"
+          {...register("trackerName", { required: true })}
+        />
+        {errors.trackerName && <span>This field is required</span>}
+        <input
+          defaultValue="Description of Tracker"
+          {...register("description")}
+        />
+          <DateTimePicker
+            onChange={dtPicker}
+            {...register("time")}
+          ></DateTimePicker>
         <input type="submit" />
       </form>
     </div>
